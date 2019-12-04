6b54d41cbbafad9114b161a820f76a9b1b750ea5
Rafa Vázquez <rafa.vazsan@gmail.com>
2019-12-04 13:27:23 +0100

    Implement concurrency with async

--- src/project/Request06Concurrent.kt
+++ src/project/Request06Concurrent.kt
@@ -1,9 +1,23 @@
 package project
 
-import kotlinx.coroutines.*
-import kotlin.coroutines.*
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
 
-suspend fun loadContributorsConcurrent(req: RequestData) : List<User> {
-    TODO()
+suspend fun loadContributorsConcurrent(req: RequestData): List<User> = coroutineScope {
+    val service = createGitHubService(req.username, req.password)
+    log.info("Loading ${req.org} repos")
+    val repos = service.listOrgRepos(req.org).await()
+    log.info("${req.org}: loaded ${repos.size} repos")
+    val userPromises = repos.map { repo ->
+        async {
+            val users = service.listRepoContributors(req.org, repo.name).await()
+            log.info("${repo.name}: loaded ${users.size} contributors")
+            users
+        }
+    }
+    val contribs = userPromises.awaitAll().flatten().aggregate()
+    log.info("Total: ${contribs.size} contributors")
+    contribs
 }
 
