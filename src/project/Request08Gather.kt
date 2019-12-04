9c3ea51ab9b8d55c5e2bd88475e258ff056cc9c6
Rafa Vázquez <rafa.vazsan@gmail.com>
2019-12-04 15:07:39 +0100

    Use channel

--- src/project/Request08Gather.kt
+++ src/project/Request08Gather.kt
@@ -1,9 +1,34 @@
 package project
 
-import kotlinx.coroutines.*
-import kotlinx.coroutines.channels.*
-import kotlin.coroutines.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-suspend fun loadContributorsGather(req: RequestData, callback: suspend (List<User>) -> Unit) {
-    TODO()
+suspend fun loadContributorsGather(req: RequestData, callback: suspend (List<User>) -> Unit) = coroutineScope {
+    val service = withContext(Dispatchers.Default) {
+        createGitHubService(req.username, req.password)
+    }
+    log.info("Loading ${req.org} repos")
+    val repos = service.listOrgRepos(req.org).await()
+    log.info("${req.org}: loaded ${repos.size} repos")
+
+    val channel = Channel<List<User>>()
+    repos.forEach { repo ->
+        launch {
+            val users = service.listRepoContributors(req.org, repo.name).await()
+            log.info("${repo.name}: loaded ${users.size} contributors")
+            channel.send(users)
+        }
+    }
+
+    var contribs = listOf<User>()
+    repeat(repos.size) {
+        val users = channel.receive()
+        contribs = (contribs + users).aggregateSlow()
+        log.info("Partial: ${contribs.size} contributors")
+        callback(contribs)
+    }
+    log.info("Total: ${contribs.size} contributors")
 }
